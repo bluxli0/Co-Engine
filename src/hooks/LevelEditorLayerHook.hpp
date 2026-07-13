27d527b96fe63b8d46d324dbefad9a700c7e286c
bluxli0 <bluxlit@gmail.com>
2026-07-13 18:27:23 +0100

    Refactor PSMEditorLayer method overrides and destructor
    Removed override specifier from update and createObject methods, and updated destructor handling for 2.2.

--- src/hooks/LevelEditorLayerHook.hpp
+++ src/hooks/LevelEditorLayerHook.hpp
@@ -8,10 +8,9 @@ using namespace geode::prelude;
 
 class PSMEditorLayer : public Modify<PSMEditorLayer, LevelEditorLayer> {
 public:
-    void update(float dt) override;
-    void createObject(int id, CCPoint pos, bool p2) override;
-    // 2.2 bindings use ~LevelEditorLayer() for destructor hooks
-    void destructor() override; 
+    void update(float dt); // Removed override
+    void createObject(int id, CCPoint pos, bool p2); // Removed override
+    ~PSMEditorLayer(); // Destructors are handled differently in 2.2
 
     void onEditorClosed();
     bool isOptimizationEnabled();
